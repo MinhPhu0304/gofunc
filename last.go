41766503b301f5626835d16de70d37632f35f8a5
minh <Lminhphu0304@gmail.com>
2020-03-05 15:39:41 +1300

    Add Last function

---
+++ last.go
@@ -0,0 +1,13 @@
+package gofunc
+
+import "reflect"
+
+// Last : Get the Last element in the array
+func Last(input interface{}) interface{} {
+	if reflect.ValueOf(input).Kind() != reflect.Array {
+		return nil
+	}
+	result := reflect.ValueOf(input)
+
+	return result.Index(result.Len() - 1)
+}
