01d5c580980886e19318319c2f73b648b8db1c1e
minh <Lminhphu0304@gmail.com>
2020-03-14 16:27:01 +1300

    Add Dec test

---
+++ test/dec_test.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+	"testing"
+
+	gofunc "github.com/MinhPhu0304/go-func"
+)
+
+func TestDec(t *testing.T) {
+	input := 20
+	expected := 19
+	actual := gofunc.Dec(input)
+	if expected != actual {
+		t.Errorf("Error the actual result is not equals expected")
+	}
+}
