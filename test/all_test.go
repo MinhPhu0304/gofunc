f377e02bc8fbaca6e48f9fe8a1bf833db6d699f4
minh <Lminhphu0304@gmail.com>
2020-04-02 11:00:47 +1300

    Add All function unit test

---
+++ test/all_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"testing"
+
+	gofunc "github.com/MinhPhu0304/gofunc"
+)
+
+func TestAllReturnTrue(t *testing.T) {
+	allEqual3 := gofunc.All(3)
+	input := []string{3, 3, 3}
+	actual := allEqual3(input)
+	expected := true
+
+	if actual != expected {
+		t.Errorf("Failed All function unitest")
+	}
+}
+
+func TestAllReturnFalse(t *testing.T) {
+	allNotEqual3 := gofunc.All(3)
+	input := []int{3, 4, 3}
+	actual := allNotEqual3(input)
+	expected := false
+
+	if actual != expected {
+		t.Errorf("Failed All function unitest")
+	}
+}
